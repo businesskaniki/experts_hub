2c81634b2d221ffe125c3ff34e9152150a2501a3
Bateyjosue <josuebatey19@gmail.com>
2023-02-01 00:48:03 +0200

    Add links to leftbar

--- src/components/leftbar/Leftbar.js
+++ src/components/leftbar/Leftbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Person from "../../assets/picon.png";
 import "./leftbar.scss";
 
@@ -18,10 +19,30 @@ const Leftbar = () => {
             <img src={Person} alt="" />
             <span>Home</span>
           </div>
-          <div className="item">
-            <img src={Person} alt="" />
-            <span>Reservation</span>
-          </div>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="item">
+              <img src={Person} alt="" />
+              <span>Technicians</span>
+            </div>
+          </Link>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="item">
+              <img src={Person} alt="" />
+              <span>Add Reservation</span>
+            </div>
+          </Link>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="item">
+              <img src={Person} alt="" />
+              <span>My Reservations</span>
+            </div>
+          </Link>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="item">
+              <img src={Person} alt="" />
+              <span>Add Technician</span>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
