711fa33148d78c0f6da6c205086bf6209e0a1b0b
Bateyjosue <josuebatey19@gmail.com>
2023-02-01 00:49:35 +0200

    update store to add the technicians store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
+import reducerTechnician from './technicians/technician'
 
 const reducer = combineReducers(
-  {}
+  {
+    technicians: reducerTechnician
+  }
 );
 
 const store = createStore(reducer, applyMiddleware(logger, thunk));
