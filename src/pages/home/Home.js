a521d4ab7a5a11ab4d8660db6ed5b17840cade64
Kehinde Onifade <onifkay@gmail.com>
2023-02-02 10:08:20 +0100

    update navbar and sidebar ui

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,75 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
     <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-    <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-  <div>Home</div>
-    </div>
   )
 }
 
