a730e94c11c71d1f58dc3f88f3e9839f9e7ce026
Bateyjosue <josuebatey19@gmail.com>
2023-02-09 16:05:20 +0200

    Add Card typography to reservation component

--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { getAllReservations } from '../../redux/reservations/reservations';
 
 const Reservation = ({ userId }) => {
   const dispatch = useDispatch();
-
   const reservations = useSelector((state) => state.reservations);
 
   useEffect(() => {
@@ -12,17 +17,29 @@ const Reservation = ({ userId }) => {
   }, [dispatch, userId]);
 
   return (
-    <ul>
+    <section>
       {reservations?.map((reservation) => (
-        <li key={reservation.id}>
-          {reservation.name}
-          {' '}
-          -
-          {' '}
-          {reservation.location}
-        </li>
+        <Card key={reservation.id}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              <Link to={`/technician/${reservation.id}`}>
+                {reservation.name}
+              </Link>
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {reservation.specialization}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Typography>{reservation.location}</Typography>
+            <Button size="small" className="btn-appointment">
+              Remove
+            </Button>
+            <Typography>{reservation.charges}</Typography>
+          </CardActions>
+        </Card>
       ))}
-    </ul>
+    </section>
   );
 };
 
