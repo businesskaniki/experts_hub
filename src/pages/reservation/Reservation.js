93b6e3aebe448967212f2fb813b1fa3fc6f366e4
Kehinde Onifade <onifkay@gmail.com>
2023-02-09 08:06:20 +0100

    try

--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllReservations } from '../../redux/reservations/reservations';
 
-const Reservation = () => (
-  <div>Reservation</div>
-);
+const Reservation = ({ userId }) => {
+  const dispatch = useDispatch();
+
+  const reservations = useSelector((state) => state.reservation.reservations);
+
+  useEffect(() => {
+    dispatch(getAllReservations(userId));
+  }, [dispatch, userId]);
+
+  console.log(reservations);
+  return (
+    <ul>
+      {reservations?.map((reservation) => (
+        <li key={reservation.id}>
+          {reservation.name}
+          {' '}
+          -
+          {' '}
+          {reservation.location}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Reservation;
