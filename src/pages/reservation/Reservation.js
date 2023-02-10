383289a18bf0c14927aa6e51b1881a92047769a0
Kehinde Onifade <onifkay@gmail.com>
2023-02-10 10:42:56 +0100

    trydelete reservation

--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -7,18 +7,23 @@ import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import { CardMedia } from '@mui/material';
 import Typography from '@mui/material/Typography';
-import { getAllReservations } from '../../redux/reservations/reservations';
+import { getAllReservations, deleteReservation } from '../../redux/reservations/reservations';
 import './reservation.css';
 
 const Reservation = ({ userId }) => {
   const dispatch = useDispatch();
   const reservations = useSelector((state) => state.reservations);
+  console.log(reservations);
 
   useEffect(() => {
     if (!reservations.length) {
       dispatch(getAllReservations(userId));
     }
   }, [dispatch, reservations.length, userId]);
+
+  const handleDelete = (id) => {
+    dispatch(deleteReservation(id));
+  };
 
   return (
     <section>
@@ -27,13 +32,13 @@ const Reservation = ({ userId }) => {
           <Card key={reservation.id} className="reserve-cont">
             <CardMedia
               sx={{ height: 200 }}
-              image={reservation.technician.image}
-              title={reservation.technician.name}
+              image={reservation.technician?.image}
+              title={reservation.technician?.name}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div" className="reserve-name">
-                <Link to={`/technician/${reservation.technician.id}`}>
-                  {reservation.technician.name}
+                <Link to={`/technician/${reservation.technician?.id}`}>
+                  {reservation.technician?.name}
                 </Link>
               </Typography>
               <Typography variant="body2" color="text.secondary">
@@ -45,7 +50,7 @@ const Reservation = ({ userId }) => {
                 Charges:
                 {' '}
                 $
-                {reservation.technician.charges}
+                {reservation.technician?.charges}
               </Typography>
               <Typography>
                 Location:
@@ -54,8 +59,8 @@ const Reservation = ({ userId }) => {
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small" className="btn-appointment">
-                Remove
+              <Button size="small" className="btn-appointment" id={reservation.id} onClick={() => handleDelete(reservation.id)}>
+                Delete
               </Button>
             </CardActions>
           </Card>
